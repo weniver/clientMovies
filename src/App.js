092595cad635d8f14445679f1da5b22be41c956e
weniver <>
2021-01-20 13:27:43 -0600

    Adds 404 component to react router and adds simple header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import MoviesScreen from "./screens/MoviesScreen.js";
 import AddMovieScreen from "./screens/AddMovieScreen.js";
+import Error404Screen from "./screens/Error404Screen.js";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <Router>
+    <Header/>
       <Switch>
         <Route exact path="/">
           <MoviesScreen />
         </Route>
         <Route exact path="/add/movie">
           <AddMovieScreen />
+        </Route>
+        <Route path="*">
+          <Error404Screen />
         </Route>
       </Switch>
     </Router>
