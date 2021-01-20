940a9fc75205f443d7401d58266483e573759aa7
weniver <>
2021-01-20 12:15:08 -0600

    Adds Basic Router with two screens

---
+++ src/screens/AddMovieScreen.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const AddMovieScreen = () => {
+  return (
+    <div className="container">
+      <h1>AddMovieScreen</h1>
+    </div>
+  );
+};
+
+export default AddMovieScreen;
