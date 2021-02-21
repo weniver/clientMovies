fc25edc26f2b45195e74c7083782bf454146288a
weniver <>
2021-02-21 12:34:06 -0600

    Refactors font awesome button foro better sizing

--- src/components/FontAwesomeButton.js
+++ src/components/FontAwesomeButton.js
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from "./FontAwesomeButton.module.scss";
 
-const FontAwesomeButton = ({ onClickHandler, fontAwesomeClasses, style }) => {
+const FontAwesomeButton = ({ fontSize, onClickHandler, fontAwesomeClasses, style }) => {
   return (
-    <div
-      onClick={() => {
-        onClickHandler();
-      }}
-      style={{ ...style }}
-      className={styles["button-wrapper"]}
-    >
-      <i
-        className={`${styles["icon"]} ${
-          fontAwesomeClasses ? fontAwesomeClasses : "far fa-question-circle"
-        }`}
-      ></i>
+    <div style={{ fontSize: `${fontSize || "2rem"}` }}>
+      <div
+        onClick={() => {
+          onClickHandler();
+        }}
+        style={{ ...style }}
+        className={styles["button-wrapper"]}
+      >
+        <i
+          className={`${styles["icon"]} ${
+            fontAwesomeClasses ? fontAwesomeClasses : "far fa-question-circle"
+          }`}
+        ></i>
+      </div>
     </div>
   );
 };
