500c88b28bb58bf576da71da0f097ac1ecbddc25
weniver <>
2021-02-21 16:25:31 -0600

    Simpler header, responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.scss";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <Link
-        className="header-link"
-        to="/"
-      >
-        <i className="fas fa-lg fa-lemon"></i>
-      </Link>
-      <Link
-        className="header-link"
-        to="/add/movie"
-      >
-        <p>+</p>
-      </Link>
+    <div className={`container-lg ${styles.header}`}>
+      <div className="row align-items-center h-100">
+        <div className="col">
+          <Link className={`${styles["header-link"]}`} to="/">
+            <i className="fas fa-lg fa-lemon"></i>
+          </Link>
+        </div>
+        <div className="col text-right">
+          <Link className={`${styles["header-link"]}`} to="/add/movie">
+            <i className="fas fa-plus"></i>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
