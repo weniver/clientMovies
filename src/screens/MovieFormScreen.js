58d388f603114765bf51a4aa81d93bd32012cff2
weniver <>
2021-02-22 16:32:59 -0600

    Working responsive form

--- src/screens/MovieFormScreen.js
+++ src/screens/MovieFormScreen.js
@@ -90,201 +90,217 @@ const MovieFormScreen = (props) => {
   };
 
   return (
-    <div className="col-8 mx-auto">
-      <h1 className="my-3">{onEditPage ? "Edit a movie" : "Add a movie:"}</h1>
-      <form
-        onKeyPress={(e) => {
-          preventSubmitOnEnterKey(e);
-        }}
-        className="row"
-        autoComplete="off"
-        onSubmit={formik.handleSubmit}
-      >
-        <div className="col-8">
-          <div className="row">
-            <div className="mb-3 col-12">
-              <SearchIMDB
-                label={"Title"}
-                value={formik.values.title}
-                onChangeHandler={formik.handleChange}
-                number={4}
-                onClickHandler={formik.setFieldValue}
-              />
-              <div className="form-text">
-                {formik.touched.title && formik.errors.title ? (
-                  formik.errors.title
-                ) : (
-                  <>&nbsp;</>
-                )}
+    <div className="container-lg">
+      <div className="col-12 col-lg-10 mx-auto">
+        <h1 className="my-3">{onEditPage ? "Edit a movie" : "Add a movie:"}</h1>
+        <form
+          onKeyPress={(e) => {
+            preventSubmitOnEnterKey(e);
+          }}
+          className="row"
+          autoComplete="off"
+          onSubmit={formik.handleSubmit}
+        >
+          <div className="col-12 col-sm-8">
+            <div className="row">
+              <div className="mb-3 col-12">
+                <SearchIMDB
+                  label={"Title"}
+                  value={formik.values.title}
+                  onChangeHandler={formik.handleChange}
+                  number={4}
+                  onClickHandler={formik.setFieldValue}
+                />
+                <div className="form-text">
+                  {formik.touched.title && formik.errors.title ? (
+                    formik.errors.title
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="mb-3 col-12">
-              <label className="form-label" htmlFor="title">
-                Director
-              </label>
-              <input
-                className="form-control"
-                id="director"
-                name="director"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.director}
-              />
-              <div className="form-text">
-                {formik.touched.director && formik.errors.director ? (
-                  formik.errors.director
-                ) : (
-                  <>&nbsp;</>
-                )}
+              <div className="mb-3 col-12">
+                <label className="form-label" htmlFor="title">
+                  Director
+                </label>
+                <input
+                  className="form-control"
+                  id="director"
+                  name="director"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.director}
+                />
+                <div className="form-text">
+                  {formik.touched.director && formik.errors.director ? (
+                    formik.errors.director
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="mb-3 col-6">
-              <label className="form-label" htmlFor="year">
-                Year
-              </label>
-              <input
-                className="form-control"
-                id="year"
-                name="year"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.year}
-              />
-              <div className="form-text">
-                {formik.touched.year && formik.errors.year ? (
-                  formik.errors.year
-                ) : (
-                  <>&nbsp;</>
-                )}
+              <div className="mb-3 col-6">
+                <label className="form-label" htmlFor="year">
+                  Year
+                </label>
+                <input
+                  className="form-control"
+                  id="year"
+                  name="year"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.year}
+                />
+                <div className="form-text">
+                  {formik.touched.year && formik.errors.year ? (
+                    formik.errors.year
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="mb-3 col-6">
-              <label className="form-label" htmlFor="country">
-                Country
-              </label>
-              <input
-                className="form-control"
-                id="country"
-                name="country"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.country}
-              />
-              <div className="form-text">
-                {formik.touched.country && formik.errors.country ? (
-                  formik.errors.country
-                ) : (
-                  <>&nbsp;</>
-                )}
+              <div className="mb-3 col-6">
+                <label className="form-label" htmlFor="country">
+                  Country
+                </label>
+                <input
+                  className="form-control"
+                  id="country"
+                  name="country"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.country}
+                />
+                <div className="form-text">
+                  {formik.touched.country && formik.errors.country ? (
+                    formik.errors.country
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="mb-3 col-6">
-              <div className="col-12 p-0">
+              <div className="mb-3 col-6">
                 <label className="form-label" htmlFor="country">
                   Watched On
                 </label>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    id="watchedOn"
+                    autoOk
+                    disableFuture
+                    disableToolbar
+                    format="d•M•yyyy"
+                    value={formik.values.watchedOn}
+                    onChange={(value) =>
+                      formik.setFieldValue("watchedOn", value)
+                    }
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                </MuiPickersUtilsProvider>
+                <div className="form-text">
+                  {formik.touched.watchedOn && formik.errors.watchedOn ? (
+                    formik.errors.watchedOn
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </div>
               </div>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker
-                  id="watchedOn"
-                  autoOk
-                  disableFuture
-                  disableToolbar
-                  format="dd/MM/yyyy"
-                  value={formik.values.watchedOn}
-                  onChange={(value) => formik.setFieldValue("watchedOn", value)}
-                  KeyboardButtonProps={{
-                    "aria-label": "change date",
-                  }}
-                />
-              </MuiPickersUtilsProvider>
-            </div>
-            <div className="mb-3 col-6">
-              <label className="form-label" htmlFor="rating">
-                Rating
-              </label>
-              <div className="row">
-                <div className="form-check ml-3">
-                  <label className="form-check-label">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="rating"
-                      value={1}
-                      checked={formik.values.rating === 1}
-                      onChange={() => {
-                        formik.setFieldValue("rating", 1);
-                      }}
-                    />
-                    1
-                  </label>
-                </div>
-                <div className="form-check ml-3">
-                  <label className="form-check-label">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="rating"
-                      value={2}
-                      checked={formik.values.rating === 2}
-                      onChange={() => {
-                        formik.setFieldValue("rating", 2);
-                      }}
-                    />
-                    2
-                  </label>
-                </div>
-                <div className="form-check ml-3">
-                  <label className="form-check-label">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="rating"
-                      value={3}
-                      checked={formik.values.rating === 3}
-                      onChange={() => {
-                        formik.setFieldValue("rating", 3);
-                      }}
-                    />
-                    3
-                  </label>
+              <div className="mb-3 col-6">
+                <label className="form-label" htmlFor="rating">
+                  Rating
+                </label>
+                <div className="row">
+                  <div className="form-check ml-3">
+                    <label className="form-check-label">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="rating"
+                        value={1}
+                        checked={formik.values.rating === 1}
+                        onChange={() => {
+                          formik.setFieldValue("rating", 1);
+                        }}
+                      />
+                      1
+                    </label>
+                  </div>
+                  <div className="form-check ml-3">
+                    <label className="form-check-label">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="rating"
+                        value={2}
+                        checked={formik.values.rating === 2}
+                        onChange={() => {
+                          formik.setFieldValue("rating", 2);
+                        }}
+                      />
+                      2
+                    </label>
+                  </div>
+                  <div className="form-check ml-3">
+                    <label className="form-check-label">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="rating"
+                        value={3}
+                        checked={formik.values.rating === 3}
+                        onChange={() => {
+                          formik.setFieldValue("rating", 3);
+                        }}
+                      />
+                      3
+                    </label>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-4">
-          {formik.values.poster === "" || formik.values.poster === "N/A" ? (
-            <div className="no-poster">
-              <i className="fas fa-lg fa-ticket-alt"></i>
-            </div>
-          ) : (
-            <>
-              <div className="poster-wrapper">
-                <img src={formik.values.poster} alt="" className="poster-img" />
+          <div className="col-12 col-sm-4 text-center">
+            {formik.values.poster === "" || formik.values.poster === "N/A" ? (
+              <div className="no-poster">
+                <i className="no-poster-icon fas fa-lg fa-ticket-alt"></i>
               </div>
-              <div
-                onClick={() => {
-                  formik.setFieldValue("poster", "");
-                }}
-                className="remove-wrapper"
-              >
-                <p className="remove-button">
-                  remove <i className="fas fa-trash-alt"></i>
-                </p>
-              </div>
-            </>
-          )}
-        </div>
-        <div className="col-3 mx-auto my-5">
-          <button type="submit" className="btn btn-primary btn-block add-movie">
-            {onEditPage ? "EDIT MOVIE" : "ADD MOVIE"}
-          </button>
-        </div>
-      </form>
+            ) : (
+              <>
+                <div>
+                  <img
+                    src={formik.values.poster}
+                    alt=""
+                    className="poster-img"
+                  />
+                  <div
+                    onClick={() => {
+                      formik.setFieldValue("poster", "");
+                    }}
+                    className="remove-wrapper"
+                  >
+                    <p className="remove-button">
+                      remove <i className="fas fa-trash-alt"></i>
+                    </p>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="col-12 col-md-4 mx-auto my-5">
+            <button
+              type="submit"
+              className="btn btn-primary btn-block add-movie"
+            >
+              {onEditPage ? "EDIT MOVIE" : "ADD MOVIE"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
