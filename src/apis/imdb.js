9174a53d7c9316ee361a3e661c2994b32ddc9056
Diego Cevallos <weniver@gmail.com>
2021-02-27 11:01:34 -0600

    Minor fix key

--- src/apis/imdb.js
+++ src/apis/imdb.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://www.omdbapi.com/",
   params: {
     r: "json",
-    apikey: process.env.REACT_APP_OMDB_KEY ?? process.env.OMDB_KEY,
+    apikey: process.env.REACT_APP_OMDB_KEY ?? "ea28638c",
   },
 });
