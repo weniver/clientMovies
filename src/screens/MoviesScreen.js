c9adcbf06f79d796609f3da7048b26151f080e1b
weniver <weniver@gmail.com>
2021-03-10 17:00:36 -0600

    transition

--- src/screens/MoviesScreen.js
+++ src/screens/MoviesScreen.js
@@ -4,38 +4,23 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchAllMovies } from "../redux/moviesSlice.js";
 import { useTransition, animated } from "react-spring";
 import TitleBanner from "../components/TitleBanner.js";
+import Loading from "../components/Loading.js";
 
 const MoviesScreen = () => {
-  const movies = useSelector((state) => state.movies.data);
+  const [movies, status] = useSelector((state) => [
+    state.movies.data,
+    state.movies.status,
+  ]);
   const dispatch = useDispatch();
   //FETCH DATA REDUX ACTIONS
   useEffect(() => {
     dispatch(fetchAllMovies());
   }, [dispatch]);
 
-  // const renderMovies = (data) => {
-  //   return data.map((movie) => {
-  //     return <MovieListItem key={movie._id} movie={movie} />;
-  //   });
-  // };
-
-  //FETCH FROM COMPONENT
-  // const [data, setData] = useState([]);
-  //
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await server.get(`/movies`);
-  //       setData(response.data);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
   const transitions = useTransition(movies, (movie) => movie._id, {
+    config: 	{ mass: 1, tension: 120, friction: 14 },
     from: { transform: "translate3d(-100vw,0,0)", opacity: 0 },
+    trail: 150,
     enter: {
       transform: "translate3d(0,0,0)",
       opacity: 1,
@@ -46,25 +31,11 @@ const MoviesScreen = () => {
     },
   });
 
-  // return (
-  //   <div>
-  //     <button onClick={addToList}>add</button>
-  //     <button onClick={removeFromList}>remove</button>
-  //
-  //     {transitions.map((hola) => {
-  //       return (
-  //         <animated.div key={hola.key} style={hola.props}>
-  //           <MovieListItem key={hola.item._id} movie={hola.item} />
-  //         </animated.div>
-  //       );
-  //     })}
-  //   </div>
-  // );
-
   return (
-    <div>
+    <div style={{ overflowY: "hidden" }}>
       <div className="container-lg p-0">
         <TitleBanner />
+        {status === "loading" && <Loading />}
         {transitions.map(({ item: movie, key, props: animation }) => {
           return (
             <animated.div key={key} style={{ ...animation }}>
